51a471f0e4f2b4edc355e3e80b18d52621e1b2fc
Matt <M@Birchbury.com>
2022-04-20 00:32:23 -0700

    Updates

---
+++ precache-manifest.5d74e5b9daf35dff4ca9613566891272.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4eb1e037fb9ec83f379",
+    "url": "/birchbury.github.io/robofriends/static/css/main.67958a04.chunk.css"
+  },
+  {
+    "revision": "b4eb1e037fb9ec83f379",
+    "url": "/birchbury.github.io/robofriends/static/js/main.b4eb1e03.chunk.js"
+  },
+  {
+    "revision": "a276464d26d8c156c662",
+    "url": "/birchbury.github.io/robofriends/static/css/1.bd99b064.chunk.css"
+  },
+  {
+    "revision": "a276464d26d8c156c662",
+    "url": "/birchbury.github.io/robofriends/static/js/1.a276464d.chunk.js"
+  },
+  {
+    "revision": "e4b5ccc416838094cd99",
+    "url": "/birchbury.github.io/robofriends/static/js/runtime~main.e4b5ccc4.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/birchbury.github.io/robofriends/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "d22e5dc7a8c9bd29eaedb49ec66c90f1",
+    "url": "/birchbury.github.io/robofriends/index.html"
+  }
+];
